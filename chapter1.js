574621224e36fbe6b91caae792c54a51247abc84
sajid <sajid.siddiqueibt@gmail.com>
2019-12-19 23:18:00 +0500

    js added

---
+++ chapter1.js
@@ -0,0 +1,20 @@
+alert("Good Morning");
+
+// Question no 2
+alert("Error ! Please Enter a Valid password");
+
+//Question 3
+alert("Welcome to JS land... \n Happy Coding");
+
+//Question 4
+alert("Welcome to JS land...");
+alert("Happy Coding!");
+
+
+
+
+//Question 5
+alert("Hello i can run JS through my web browser console");
+
+//Question 3
+//Question 3
